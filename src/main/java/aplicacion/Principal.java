eec7948847a344b957812c0097ec221a09420852
Danii <danielfernandezlopez75@gmail.com>
2022-05-04 16:30:13 +0200

    Desarrollo Practica 4 2.1

--- src/main/java/aplicacion/Principal.java
+++ src/main/java/aplicacion/Principal.java
@@ -1,10 +1,18 @@
 package aplicacion;
-
 import dominio.CalculadoraDistancias;
 
+/**
+ * Clase principal de la aplicación.
+ *
+ * @author Daniel Fernández López
+ */
 public class Principal {
+    /**
+     * Método principal del programa que llama al método distanciaEdicion con el que se calcula la distancia de edición entre dos palabras.
+     *
+     * @param args Argumentos de la línea de comandos.
+     */
     public static void main(String[] args) {
-
-        System.out.println("Distancia: " + CalculadoraDistancias.distanciaEdicicion("JESUS", "JUAN"));
+        System.out.println("Distancia: " + CalculadoraDistancias.distanciaEdicicion("Patata", "Patete"));
     }
 }
--- src/main/java/dominio/CalculadoraDistancias.java
+++ src/main/java/dominio/CalculadoraDistancias.java
@@ -1,54 +1,45 @@
 package dominio;
 
+/**
+ * Clase que calcula la distancia entre dos puntos
+ *
+ * @author Daniel Fernández López
+ */
 public class CalculadoraDistancias {
 
     /**
-     * Calcula la distancia de edición, tal y como la expone
-     * Jurafsky entre las cadenas ` s1` y ` s2`.
+     * Método que calcula el mínimo de 3 valores.
+     *
+     * @param a valor que contiene el caso de deletion.
+     * @param b valor que contiene el caso de insertion.
+     * @param c valor que contiene el caso de substitution.
+     * @return el valor mínimo de los 3 valores.
      */
-
-    /* public static int distancia(String cadena1, String cadena2) {
-        int distancia;
-         int m = cadena1.length();
-         int n = cadena2.length();
-         int[][] d = new int[m + 1][n + 1];
-         for (int i = 0; i <= m; i++) {
-             d[i][0] = i;
-         }
-         for (int j = 0; j <= n; j++) {
-             d[0][j] = j;
-         }
-         for (int j = 1; j <= n; j++) {
-             for (int i = 1; i <= m; i++) {
-                 if (cadena1.charAt(i - 1) == cadena2.charAt(j - 1)) {
-                     d[i][j] = d[i - 1][j - 1];
-                 }
-                 else {
-                     d[i][j] = Math.min(d[i - 1][j] + 1, Math.min(d[i][j - 1] + 1, d[i - 1][j - 1] + 1));
-                 }
-             }
-         }
-         distancia = d[m][n];
-         return distancia;
-    } */
-
     public static int min(int a, int b, int c) {
         return Math.min(Math.min(a, b), c);
     }
 
+    /**
+     * Método que calcula la distancia de edicion entre dos palabras (String).
+     *
+     * @param X palabra 1
+     * @param Y palabra 2
+     * @return distancia de edicion
+     */
     public static int distanciaEdicicion(String X, String Y) {
         int n = X.length();
         int m = Y.length();
-        int D[][] = new int[n + 1][m + 1];
-        for (int i = 0; i <= n; i++) {
+        int D[][] = new int[n + 1][m + 1];  // inicializa matriz de dos dimensiones, con n+1 filas y m+1 columnas
+        for (int i = 0; i <= n; i++) {     // inicializa la primera fila
             D[i][0] = i;
         }
-        for (int j = 0; j <= m; j++) {
+        for (int j = 0; j <= m; j++) {    // inicializa la primera columna
             D[0][j] = j;
         }
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                D[i][j] = min(D[i - 1][j] + 1, D[i][j - 1] + 1, D[i - 1][j - 1] + (X.charAt(i - 1) == Y.charAt(j - 1) ? 0 : 1));
+        for (int i = 1; i <= n; i++) {   // recorre la matriz por filas
+            for (int j = 1; j <= m; j++) {  // recorre la matriz por columnas
+                D[i][j] = min(D[i - 1][j] + 1, D[i][j - 1] + 1, D[i - 1][j - 1] + (X.charAt(i - 1) == Y.charAt(j - 1) ? 0 : 1 + 1));
+                // calcula la distancia de edición con los distintos casos: a (deletion), b (insertion), c (substitution).
             }
         }
         return D[n][m];
